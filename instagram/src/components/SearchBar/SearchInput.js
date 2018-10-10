7d101a4ed5d3eaef2d8f9b3cffea21e3a91cc541
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-09 21:57:05 -0400

    added SearchInput.js file added functional component for SearchInput

---
+++ instagram/src/components/SearchBar/SearchInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchInput = (props) => {
+  return (
+    <form action='' method='get'>
+      <input type='text' name='SearchInput'></input>
+    </form>
+  );
+}
+
+export default SearchInput;
